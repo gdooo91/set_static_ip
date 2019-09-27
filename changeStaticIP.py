0b12870a67a713138bf6ccd9b2e2bedb62ccb12e
gdooo91 <gdooo91@gmail.com>
2019-09-26 20:28:09 -0700

    added (setIP) to the 'selectedIterface' Class

--- changeStaticIP.py
+++ changeStaticIP.py
@@ -7,16 +7,27 @@ from netsh_set import getInterfaceNames
 
 
 class selectedInterface:
-    def __init__(self, Name= None, ID = None):
+    def __init__(self, Name= None, ID = None, IP = None):
         self.Name = Name
         self.ID = ID
+        self.IP = IP
 
-    def setName(self,newName):
+    def setName(self,newName): # ToDo: safe to remove ?
         self.Name = newName
         print(self.Name)
 
-    def setID(self, newID):
+    def setID(self, newID): # ToDo: safe to remove ?
         self.ID = newID
+    def setIP(self, newName, newIP):
+        self.Name = newName
+        self.IP = newIP
+        defaultSubnetMaskStr = "255.255.255.0"
+
+        setCMD = "netsh interface ipv4 set address name= " + self.Name + " static " +self.IP+ " " + defaultSubnetMaskStr + " "
+        print(setCMD)
+
+        
+
 
 interface = selectedInterface()
 
@@ -30,13 +41,13 @@ for i in range(len(intefaceList)):
 ## command that will chagne the ip address
 # netsh interface ipv4 set address name="YOUR INTERFACE NAME" static IP_ADDRESS SUBNET_MASK GATEWAY
 
-interfaceNumb = input("Select interface number: ")
-try: 
-    print("you selected: ",intefaceList[int(interfaceNumb)])
-except:
-    print(interfaceNumb,"Error!!\n")
-newIpAddress = [0,0,0,0]
-newSubnetMask = [0,0,0,0]
+# interfaceNumb = input("Select interface number: ")
+# try: 
+#     print("you selected: ",intefaceList[int(interfaceNumb)])
+# except:
+#     print(interfaceNumb,"Error!!\n")
+# newIpAddress = [0,0,0,0]
+# newSubnetMask = [0,0,0,0]
 
 # newIpAddressStr = input ("Enter new Static IP 'saprate by .': ")
 # newSubnetMaskStr = input ("Enter new Subnet Mask 'saprate by .': ")
@@ -59,10 +70,6 @@ yPad = 10
 
 RAWnum = 0 
 
-# str_interface = tk.StringVar()
-# cmbox_interface = ttk.Combobox(root, textvariable=str_interface, 
-#     values=intefaceList)
-# cmbox_interface.grid(row=RAWnum, column=0,columnspan =  3,rowspan=6, sticky=(tk.N,tk.S,tk.E,tk.W))
 print(root.winfo_width())
 lb_interface = tk.Listbox()
 lb_interface.configure(width = 50)
@@ -74,21 +81,17 @@ lb_interface.grid(column = 1)
 for i in range(0,len(intefaceList),2):
      lb_interface.itemconfigure(i, background='#f0f0ff')
 
-
-
-# interface_listBox = tk.Listbox(root, height=len(intefaceList))
-# interface_listBox.grid(column=RAWnum, row=0, rowspan=6, sticky=(tk.N,tk.S,tk.E,tk.W))
-
-# interface_listBox.bind('<<ListboxSelect>>', intefaceList) #getInterfaceNames) #showPopulation)
-# interface_listBox.bind('<Double-1>',print("bind"))#sendGift)
-# for i in range(0,len(intefaceList),2):
-#     interface_listBox.itemconfigure(i, background='#f0f0ff')
-
 RAWnum+=1
-btn_SetIP = tk.Button(root, text = 'Select interface' , command = lambda:interface.setName(lb_interface.selection_get()))#,font = fontStyle)
-btn_SetIP.grid(row = 10 , column = 2, pady=yPad, padx = xPad, sticky=tk.E+tk.W)
+btn_SetIP = tk.Button(root, text = 'Select interface' , command = lambda:interface.setIP(lb_interface.selection_get(),var_IP.get()))#,font = fontStyle)
+btn_SetIP.grid(row = RAWnum , column = 2, pady=yPad, padx = xPad, sticky=tk.E+tk.W)
 btn_SetIP.config( height = 1, width = 15 )
+
 RAWnum+=1
+lbl_IP = tk.Label(root, text ="enter new IP address", font = fontStyleL)
+lbl_IP.grid(row = RAWnum, column = 0, pady=yPad, padx = xPad, sticky=tk.E+tk.W)
+var_IP = tk.StringVar()
+entry_IP = tk.Entry(root, textvariable =var_IP, width = 15 , font = fontStyle)
+entry_IP.grid(row = RAWnum, column = 2, pady=yPad, padx = xPad, sticky=tk.E+tk.W)
 
 
 root.mainloop( )
